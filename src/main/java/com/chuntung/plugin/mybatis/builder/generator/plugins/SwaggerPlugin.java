da39f19c5394466ee86b30f852d826ac8828ad28
Gao Zhengyuan <gzya910712@163.com>
2023-11-06 10:25:24 +0800

    增加ApiPropertyReference支持

--- src/main/java/com/chuntung/plugin/mybatis/builder/generator/plugins/SwaggerPlugin.java
+++ src/main/java/com/chuntung/plugin/mybatis/builder/generator/plugins/SwaggerPlugin.java
@@ -12,7 +12,8 @@ import java.util.List;
 
 public class SwaggerPlugin extends PluginAdapter {
 
-    private final static FullyQualifiedJavaType ANNOTATION_TYPE = new FullyQualifiedJavaType("io.swagger.annotations.ApiModelProperty");
+    private final static FullyQualifiedJavaType ANNOTATION_TYPE_1 = new FullyQualifiedJavaType("io.swagger.annotations.ApiModelProperty");
+    private final static FullyQualifiedJavaType ANNOTATION_TYPE_2 = new FullyQualifiedJavaType("com.uxq.common.annotations.ApiPropertyReference");
 
     @Override
     public boolean validate(List<String> warnings) {
@@ -20,7 +21,20 @@ public class SwaggerPlugin extends PluginAdapter {
     }
 
     private void populateSwaggerAnnotation(TopLevelClass topLevelClass) {
-        topLevelClass.addImportedType(ANNOTATION_TYPE);
+
+        for (Field field : topLevelClass.getFields()) {
+            for (String annotation : field.getAnnotations()) {
+                if (annotation != null) {
+                    if (annotation.startsWith("@ApiModelProperty")) {
+                        topLevelClass.addImportedType(ANNOTATION_TYPE_1);
+                    } else if (annotation.startsWith("@ApiPropertyReference")) {
+                        topLevelClass.addImportedType(ANNOTATION_TYPE_2);
+                    }
+                }
+            }
+        }
+
+
     }
 
     @Override
@@ -29,7 +43,6 @@ public class SwaggerPlugin extends PluginAdapter {
         populateSwaggerAnnotation(topLevelClass);
         return true;
     }
-
 
     @Override
     public boolean modelPrimaryKeyClassGenerated(TopLevelClass topLevelClass,
@@ -49,11 +62,23 @@ public class SwaggerPlugin extends PluginAdapter {
     public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
         String remarks = introspectedColumn.getRemarks();
         if (StringUtil.stringHasValue(remarks)) {
-            int index = remarks.indexOf("\r\n");
-            if (index > 0) {
-                remarks = remarks.substring(0, index);
+            String enumClass = null;
+            String remarkValue = remarks;
+            int indexSee = remarks.indexOf("@see");
+            if (indexSee > 0) {
+                enumClass = remarks.substring(indexSee + 5);
+
             }
-            field.addAnnotation("@ApiModelProperty(\"" + remarks + "\")");
+            int indexReturn = remarks.indexOf("\r\n");
+            if (indexReturn > 0) {
+                remarkValue = remarks.substring(0, indexReturn);
+            }
+
+            if (enumClass != null) {
+                field.addAnnotation("@ApiPropertyReference(value = \"" + remarkValue + "\", referenceClazz = " + enumClass + ".class)");
+            } else {
+                field.addAnnotation("@ApiModelProperty(\"" + remarkValue + "\")");
+            }
         }
         return true;
     }
